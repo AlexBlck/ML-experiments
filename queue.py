dd73d8a565b6d410ea91b1c6dd69fc160c989b2c
AlexBlck <jermakov.aleksandr@gmail.com>
2018-06-07 19:59:00 +0200

    Basic box, ready to learn

--- queue.py
+++ queue.py
@@ -11,29 +11,26 @@ import itertools
 import pygame
 from pygame.locals import *
 from pygame.color import *
-    
 import numpy as np
-
 import pymunk
+from PIL import Image
 from pymunk.vec2d import Vec2d
 import pymunk.pygame_util 
-
 import os
+import skimage as sk
+from skimage import color as skcolor
 
 current_path = os.path.dirname(__file__)
 moves = [0,0,0,0] #left, rght, up, down
 directions = list(itertools.product([-1,0,1], repeat=2))
-PLAYER_VELOCITY = 10
+PLAYER_VELOCITY = 20
+width, height = 28, 28
+hole_width = 8
+fps = 120
+dt = 1./fps
+
 def flipy(y):
     return -y+height
-
-def cpfclamp(f, min_, max_):
-    """Clamp f between min and max"""
-    return min(max(f, min_), max_)
-
-def cpflerpconst(f1, f2, d):
-    """Linearly interpolate from f1 to f2 by no more than d."""
-    return f1 + cpfclamp(f2 - f1, -d, d)
 
 def move(moves, body):
 
@@ -51,94 +48,41 @@ def move(moves, body):
 
         body.velocity = target_vx, target_vy
 
-width, height = 690,400
-fps = 60
-dt = 1./fps
-PLAYER_VELOCITY = 20
-
-
-
 def main():
 
     ### PyGame init
     pygame.init()
     screen = pygame.display.set_mode((width,height)) 
-
+    frame_number = 0
     clock = pygame.time.Clock()
     running = True
-    font = pygame.font.SysFont("Arial", 16)
     
     ### Physics stuff
     space = pymunk.Space()   
     space.gravity = 0, 0
     draw_options = pymunk.pygame_util.DrawOptions(screen)
 
+    hole_location = np.random.random_integers(1,18)
     # box walls 
-    static = [pymunk.Segment(space.static_body, (10, 50), (300, 50), 10)
-                , pymunk.Segment(space.static_body, (300, 50), (325, 50), 10)
-                , pymunk.Segment(space.static_body, (325, 50), (350, 50), 10)
-                , pymunk.Segment(space.static_body, (350, 50), (375, 50), 10)
-                , pymunk.Segment(space.static_body, (375, 50), (680, 50), 10)
-                , pymunk.Segment(space.static_body, (680, 50), (680, 370), 10)
-                , pymunk.Segment(space.static_body, (680, 370), (10, 370), 10)
-                , pymunk.Segment(space.static_body, (10, 370), (10, 50), 10)
+    static = [pymunk.Segment(space.static_body, (0, 0), (width, 0), 2),
+              pymunk.Segment(space.static_body, (0, 0), (0, height), 1),
+              pymunk.Segment(space.static_body, (width, 0), (width, height), 2),
+              pymunk.Segment(space.static_body, (0, height), (hole_location, height), 1),
+              pymunk.Segment(space.static_body, (hole_location+hole_width, height), (width, height), 1)
                 ]  
     
-    # rounded shape
-    rounded = [pymunk.Segment(space.static_body, (500, 50), (520, 60), 3)
-                , pymunk.Segment(space.static_body, (520, 60), (540, 80), 3)
-                , pymunk.Segment(space.static_body, (540, 80), (550, 100), 3)
-                , pymunk.Segment(space.static_body, (550, 100), (550, 150), 3)
-                ]
-                
-    # static platforms
-    platforms = [pymunk.Segment(space.static_body, (170, 50), (270, 150), 3)
-                , pymunk.Segment(space.static_body, (270, 100), (300, 100), 5)
-                , pymunk.Segment(space.static_body, (400, 150), (450, 150), 3)
-                , pymunk.Segment(space.static_body, (400, 200), (450, 200), 3)
-                , pymunk.Segment(space.static_body, (220, 200), (300, 200), 3)
-                , pymunk.Segment(space.static_body, (50, 250), (200, 250), 3)
-                , pymunk.Segment(space.static_body, (10, 370), (50, 250), 3)
-                ]
-    
-    for s in static + platforms+rounded:
+    for s in static:
         s.friction = 1.
         s.group = 1
-    space.add(static, platforms+rounded)
-    
+    space.add(static)
+
     # player
-    body_radius = 10
+    body_radius = 2
     body = pymunk.Body(1, pymunk.inf)
-    body.position = 100,100
+    body.position = np.random.random_integers(2, 16, (2, 1))
     feet = pymunk.Circle(body, body_radius, (0, 0))
     space.add(body, feet)
 
-    def draw_ray(start, radius, thickness):
-         ### Clear screen
-        screen.fill(pygame.color.THECOLORS["black"])
-        
-        ### Draw stuff
-        space.debug_draw(draw_options)
-
-        for direction in directions:
-            line_end = start + (direction[0]*(width/2), direction[1]*(height/2))
-            line_start = start + (direction[0]*(radius + 0.5), direction[1]*(radius + 0.5))
-            segment_q = space.segment_query_first(line_start, line_end, thickness, pymunk.ShapeFilter())
-            if segment_q:
-                contact_point = segment_q.point
-                line = pymunk.Segment(space.static_body, line_start, contact_point, thickness)
-                line.sensor = True
-                line.body.position = 0,0
-                line_length = math.sqrt((contact_point[0] - line_start[0])**2 + 
-                                        (contact_point[1] - line_start[1])**2)
-                print(line_length)
-
-                p1 = line_start.x, flipy(line_start.y)
-                p2 = contact_point.x, flipy(contact_point.y)
-                pygame.draw.lines(screen, THECOLORS["green"], False, [p1, p2])
-
-
-    frame_number = 0
     while running:
             
         for event in pygame.event.get():
@@ -146,39 +90,33 @@ def main():
                 event.type == KEYDOWN and (event.key in [K_ESCAPE, K_q]):  
                 running = False
             elif event.type == KEYDOWN and event.key == K_p:
-                pygame.image.save(screen, "platformer.png")
+                img = pygame.image.tostring(screen, 'RGB')
+                img = Image.frombytes('RGB', (width, height), img)
+                original = sk.img_as_float(img)
+                original = skcolor.rgb2gray(original)
+                original = original.flatten()
+                print(original.shape)
+                
+        ### Clear screen
+        screen.fill(pygame.color.THECOLORS["black"])
+            
+        ### Draw stuff
+        space.debug_draw(draw_options)
 
-        
-        draw_ray(body.position, 10, 0.2)
-
+        ### Move
         moves = np.random.randint(0,2,(4,1))
-        
         move(moves, body)
         
-        target_vx = 0
-        target_vy = 0
-
-        keys = pygame.key.get_pressed()
-        if (keys[K_LEFT]):
-            target_vx -= PLAYER_VELOCITY
-        if (keys[K_RIGHT]):
-            target_vx += PLAYER_VELOCITY
-        if (keys[K_DOWN]):
-            target_vy -= PLAYER_VELOCITY
-        if (keys[K_UP]):
-            target_vy += PLAYER_VELOCITY
-
-        body.velocity = target_vx, target_vy
-        
         # Info and flip screen
-        screen.blit(font.render("fps: " + str(clock.get_fps()), 1, THECOLORS["white"]), (0,0))
         pygame.display.flip()
         frame_number += 1
+
         ### Update physics
-        
         space.step(dt)
-        
         clock.tick(fps)
+
+        #if body.position[1] > height/4:
+        #    return frame_number
 
 
 if __name__ == '__main__':
